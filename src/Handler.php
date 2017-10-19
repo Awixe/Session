d3a24bcb854c02aa68ddfb971bdfb5153ab2dc3a
Nicholas English <awixe443@gmail.com>
2017-10-19 04:44:23 +0000

    Apply fixes from StyleCI

--- src/Handler.php
+++ src/Handler.php
@@ -1,28 +1,37 @@
 <?php
 /**
- * Session module - A session handler that acts as a wrapper
- * 
+ * Session module - A session handler that acts as a wrapper.
+ *
  * @copyright (c) 2017 Nicholas English
  * @license GPL-3.0
+ *
  * @link    https://github.com/Awixe/Session/blob/master/LICENSE
  */
+
 namespace Awixe\Module\Session;
-class Handler implements HandlerInterface {
-    public function set(string $name, $value = null) {
+
+class Handler implements HandlerInterface
+{
+    public function set(string $name, $value = null)
+    {
         if (!is_null($value)) {
             $_SESSION[$name] = $value;
         }
     }
-    public function get(string $name, $defaultReturnValue = null) {
+
+    public function get(string $name, $defaultReturnValue = null)
+    {
         if (isset($_SESSION[$name])) {
             return $_SESSION[$name];
         }
+
         return $defaultReturnValue;
     }
-    public function del(string $name) {
+
+    public function del(string $name)
+    {
         if (isset($_SESSION[$name])) {
             unset($_SESSION[$name]);
         }
     }
 }
-?>
--- src/HandlerInterface.php
+++ src/HandlerInterface.php
@@ -1,15 +1,20 @@
 <?php
 /**
- * Session module - A session handler that acts as a wrapper
- * 
+ * Session module - A session handler that acts as a wrapper.
+ *
  * @copyright (c) 2017 Nicholas English
  * @license GPL-3.0
+ *
  * @link    https://github.com/Awixe/Session/blob/master/LICENSE
  */
+
 namespace Awixe\Module\Session;
-interface HandlerInterface {
+
+interface HandlerInterface
+{
     public function set(string $name, $value = null);
+
     public function get(string $name, $defaultReturnValue = null);
+
     public function del(string $name);
 }
-?>
