b9b924e60d27dce62c321a3a56e78eb776a98554
Nicholas English <awixe443@gmail.com>
2017-10-18 23:49:34 -0400

    Build the base structure of the handler

--- src/Handler.php
+++ src/Handler.php
@@ -1,4 +1,18 @@
 <?php
-
-
+namespace Awixe\Module\Session;
+class Handler
+{
+    public function set()
+    {
+      
+    }
+    public function get()
+    {
+      
+    }
+    public function del()
+    {
+      
+    }
+}
 ?>
