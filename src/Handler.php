dfd738788cfe9abf6129096ccbc074cb0fd2e7c7
Nicholas English <awixe443@gmail.com>
2017-10-18 23:59:36 -0400

    Finalize the session handler

--- src/Handler.php
+++ src/Handler.php
@@ -1,19 +1,19 @@
 <?php
 namespace Awixe\Module\Session;
-class Handler
-{
-    public function set(string $name)
-    {
-      
+class Handler {
+    public function set(string $name, $value = null) {
+        if (!is_null($value)) {
+            $_SESSION[$name] = $value;
+        }
     }
-    public function get(string $name)
-    {
-      
+    public function get(string $name, $defaultReturnValue = null) {
+        if (isset($_SESSION[$name])) {
+            return $_SESSION[$name];
+        }
+        return $defaultReturnValue;
     }
-    public function del(string $name)
-    {
-        if (isset($_SESSION[$name]))
-        {
+    public function del(string $name) {
+        if (isset($_SESSION[$name])) {
             unset($_SESSION[$name]);
         }
     }
