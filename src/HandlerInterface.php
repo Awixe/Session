2bae8bda7ff455d4cc2f42e35d0f647f85ec9d5f
Nicholas English <awixe443@gmail.com>
2017-10-19 00:02:59 -0400

    Add the handler interface

--- src/HandlerInterface.php
+++ src/HandlerInterface.php
@@ -1,1 +1,8 @@
-
+<?php
+namespace Awixe\Module\Session;
+interface HandlerInterface {
+    public function set(string $name, $value = null);
+    public function get(string $name, $defaultReturnValue = null);
+    public function del(string $name);
+}
+?>
